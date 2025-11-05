539eb836fc6b8c605ea777b8d9c9baeade38698b
Aaron Bergmann <sorcery.ramrod.9d@icloud.com>
2025-11-05 04:11:18 +0000

    Update TODO.md & resolve_target.py

--- modules/resolve_target.py
+++ modules/resolve_target.py
@@ -2,14 +2,16 @@
 resolve_target.py
 
 Module handling the functionality of the 'Resolve IP/Hostname' menu option.
+Return a tuple containing the IP and hostname for a given target.
 """
 
 from .helpers import resolve
 from typing import Tuple
 
-def resolve_target(target: str) -> Tuple[str, str]:
+def resolve_target(target: str) -> Tuple[str, str] | Tuple[None, None]:
     """
-    Return a tuple of containing the IP and hostname for a given target.
+    See docstring at the top of the document for information
+    about this modules functionality...
 
     Args:
         target (str): The target to resolve.
@@ -17,4 +19,9 @@ def resolve_target(target: str) -> Tuple[str, str]:
     Returns:
         Tuple[str, str]: The IP and hostname.
     """
+
+    # determine if hostname, if hostname, lookup ip
+    # if ip, if ip, lookup hostname
+    # if neither, ...
+
     return target, target
