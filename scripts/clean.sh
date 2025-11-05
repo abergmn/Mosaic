fd76ac665603676cb3364cdd7c287d1f5a1fd03e
Aaron Bergmann <sorcery.ramrod.9d@icloud.com>
2025-11-05 03:29:40 +0000

    Update clean.sh & .gitignore

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -5,6 +5,9 @@ echo "[clean.sh]:  Starting clean..."
 rm -rf ./modules/__pycache__
 echo "[clean.sh]: Removed './modules/__pycache__'"
 
+rm -rf ./modules/helpers/__pycache__
+echo "[clean.sh]: Removed './modules/helpers/__pycache__'"
+
 rm -rf ./modules/utils/__pycache__
 echo "[clean.sh]: Removed './modules/utils/__pycache__'"
 
